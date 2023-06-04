48945febb03cd2d1886f32b073e582c3213f7b05
Nicola Hermann <nicola.hermann@bluewin.ch>
2023-06-04 18:39:25 +0200

    Change Model to CPU

--- src/export_model.py
+++ src/export_model.py
@@ -7,9 +7,10 @@ CKPT = r"logs\lightning_logs\version_0\checkpoints\epoch=199-step=200.ckpt"
 
 model = BrandsDetector.load_from_checkpoint(CKPT)
 model.eval()
+model.to("cpu")
 
-example = torch.rand(1, 3, 224, 224).to("cuda")
+example = torch.rand(1, 3, 224, 224).to("cpu")
 traced_script_module = torch.jit.trace(model.model, example)
 traced_script_module_optimized = optimize_for_mobile(traced_script_module)
-traced_script_module_optimized._save_for_lite_interpreter("model.ptl")
+traced_script_module_optimized._save_for_lite_interpreter("model_cpu.ptl")
 
